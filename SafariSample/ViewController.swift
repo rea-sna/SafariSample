9605c9b34ae12ceb213d4fd2327839d57d3a5785
Reo Shigeno <reoshigeno0708@gmail.com>
2020-06-06 23:53:52 +0900

    Initial Commit

---
+++ SafariSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafariSample
+//
+//  Created by 繁野怜央 on 20200606.
+//  Copyright © 2020 Reo.S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
